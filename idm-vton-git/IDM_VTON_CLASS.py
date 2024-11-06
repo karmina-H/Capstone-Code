3c6463ce4088ae05121346240a8e1bec0074fde3
Karmina <gusdbf1114@Gmail.com>
2024-11-06 10:17:25 +0900

    idm_vton_class주석끝

--- idm-vton-git/IDM_VTON_CLASS.py
+++ idm-vton-git/IDM_VTON_CLASS.py
@@ -37,20 +37,31 @@ editclass = IDM_VTON()
 edtied_image,masked_image = editclass(매개변수)
 '''
 
+
+#pil이미지를 입력받아서 바이너리 마스크로 변환하는 함수
 def pil_to_binary_mask(pil_image, threshold=0):
+    #pil이미지를 넘파이배열로 변환
     np_image = np.array(pil_image)
+    #grayscale_image는 pil이미지에서 그레이스케일이미지 즉 각 픽셀을 0~255의 단일채널로 바꿈
     grayscale_image = Image.fromarray(np_image).convert("L")
+    #그레이스 케일 이미지로 변환된 각 픽셀이 기준값보다 크면 True, 작으면 False로 변환된 binary_mask를 생성
     binary_mask = np.array(grayscale_image) > threshold
+    #binary_mask와 동일한 크기의 배열을 생성하고, 모든 값을 0으로 초기화한 mask선언
     mask = np.zeros(binary_mask.shape, dtype=np.uint8)
+    #바이너리 마스크에서 정보추출해서 mask의 마스킹으로 활용 즉 mask = 이미지의 마스크된 정보
     for i in range(binary_mask.shape[0]):
         for j in range(binary_mask.shape[1]):
             if binary_mask[i,j] == True :
                 mask[i,j] = 1
+    #mask 값을 255로 확장하여 최종 마스크를 생성하고, 데이터 타입을 uint8로 변환
     mask = (mask*255).astype(np.uint8)
+    #mask 배열을 PIL 이미지로 변환하여 output_mask에 저장
     output_mask = Image.fromarray(mask)
+    #결과반환
     return output_mask
 
 
+#pretrained모델 불러오는 부분
 base_path = 'yisol/IDM-VTON'
 example_path = os.path.join(os.path.dirname(__file__), 'example')
 
@@ -134,7 +145,7 @@ pipe = TryonPipeline.from_pretrained(
 pipe.unet_encoder = UNet_Encoder
         
 
-
+#이미지 diffusion 실행하는 함수
 def start_tryon(human_img,masked_img,garm_img,garment_des,is_checked,is_checked_crop,denoise_steps,seed):
     openpose_model.preprocessor.body_estimation.model.to(device)
     pipe.to(device)
@@ -159,15 +170,20 @@ def start_tryon(human_img,masked_img,garm_img,garment_des,is_checked,is_checked_
 
 
     if is_checked:
+        #모델을 사용해서 마스킹생성
         keypoints = openpose_model(human_img.resize((384,512)))
         model_parse, _ = parsing_model(human_img.resize((384,512)))
         mask, mask_gray = get_mask_location('hd', "upper_body", model_parse, keypoints)
+        #마스크도 이미지와 동일한크기로 만들어줌
         mask = mask.resize((768,1024))
     else:
+        #사용자가 직접 마스킹된 이미지를 넣어줄때
         mask = pil_to_binary_mask(masked_img.convert("RGB").resize((768, 1024)))
         # mask = transforms.ToTensor()(mask)
         # mask = mask.unsqueeze(0)
+        #mask를 텐서로 변환하고 반전시킨 후, human_img 텐서와 곱하여 마스크가 적용된 이미지를 생성
     mask_gray = (1-transforms.ToTensor()(mask)) * tensor_transfrom(human_img)
+    #텐서를 PIL 이미지로 다시 변환
     mask_gray = to_pil_image((mask_gray+1.0)/2.0)
 
 
@@ -175,9 +191,10 @@ def start_tryon(human_img,masked_img,garm_img,garment_des,is_checked,is_checked_
     human_img_arg = convert_PIL_to_numpy(human_img_arg, format="BGR")
      
     
-
+    #densepose모델불러오기
     args = apply_net.create_argument_parser().parse_args(('show', './configs/densepose_rcnn_R_50_FPN_s1x.yaml', './ckpt/densepose/model_final_162be9.pkl', 'dp_segm', '-v', '--opts', 'MODEL.DEVICE', 'cuda'))
     # verbosity = getattr(args, "verbosity", None)
+    #포즈이미지 생성
     pose_img = args.func(args,human_img_arg)    
     pose_img = pose_img[:,:,::-1]    
     pose_img = Image.fromarray(pose_img).resize((768,1024))
@@ -187,8 +204,9 @@ def start_tryon(human_img,masked_img,garm_img,garment_des,is_checked,is_checked_
         with torch.cuda.amp.autocast():
             with torch.no_grad():
                 prompt = "model is wearing " + garment_des
-                negative_prompt = "monochrome, lowres, bad anatomy, worst quality, low quality"
+                negative_prompt = "monochrome, lowres, bad anatomy, worst quality, low quality, inaccurate"
                 with torch.inference_mode():
+                    #pipe에 있는encode_prompt함수이용해서 프롬프트임베딩생성
                     (
                         prompt_embeds,
                         negative_prompt_embeds,
@@ -200,7 +218,7 @@ def start_tryon(human_img,masked_img,garm_img,garment_des,is_checked,is_checked_
                         do_classifier_free_guidance=True,
                         negative_prompt=negative_prompt,
                     )
-                                    
+                    #옷사진 설명에 대한 프롬프트 임베딩생성.
                     prompt = "a photo of " + garment_des
                     negative_prompt = "monochrome, lowres, bad anatomy, worst quality, low quality"
                     if not isinstance(prompt, List):
@@ -221,10 +239,13 @@ def start_tryon(human_img,masked_img,garm_img,garment_des,is_checked,is_checked_
                         )
 
 
-
+                    #pose_img와 garm_img를 텐서로 변환하고 맨 처음 차원에 배치차원을 추가
                     pose_img =  tensor_transfrom(pose_img).unsqueeze(0).to(device,torch.float16)
                     garm_tensor =  tensor_transfrom(garm_img).unsqueeze(0).to(device,torch.float16)
+                    #생서기 초기화해서 시드값고정 = 매번 동일한 이미지 생성가능. -> 보류
                     generator = torch.Generator(device).manual_seed(seed) if seed is not None else None
+
+                    #주어진 변수들을 바탕으로 파이프라인실행(return값은 diffusion된 이미지)
                     images = pipe(
                         prompt_embeds=prompt_embeds.to(device,torch.float16),
                         negative_prompt_embeds=negative_prompt_embeds.to(device,torch.float16),
@@ -244,11 +265,13 @@ def start_tryon(human_img,masked_img,garm_img,garment_des,is_checked,is_checked_
                         guidance_scale=2.0,
                     )[0]
 
+    #크롭되었으면 복구하는데 우리 크롭안할꺼
     if is_checked_crop:
         out_img = images[0].resize(crop_size)        
         human_img_orig.paste(out_img, (int(left), int(top)))    
         return human_img_orig, mask_gray
     else:
+        #diffusion된 이미지와 마스크된이미지를 반환.
         return images[0], mask_gray
     # return images[0], mask_gray
 
